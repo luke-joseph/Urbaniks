a4d1520fe8db05c60b7c9d3324f25fca3de517c8
Luke Joseph <illuzual@gmail.com>
2019-06-17 11:24:06 +0200

    dev responsive social links developed

--- resources/views/layouts/socials.blade.php
+++ resources/views/layouts/socials.blade.php
@@ -1,34 +1,62 @@
-        <div id="sticky-error-message" style="top: 240px;" class="fixed z-50 right-0">
-          <ul>
-            <li>
-              <div class="flex">
-                <div class="hidden">
-                  facebook
-                </div>
-                <div class="h-12 w-12 bg-blue-400">
-
-                </div>
-              </div>
-            </li>
-            <li>
-              <div class="flex mt-4">
-                <div class="hidden">
-                  facebook
-                </div>
-                <div class="h-12 w-12 bg-blue-400">
-
-                </div>
-              </div>
-            </li>
-            <li>
-              <div class="flex mt-4">
-                <div class="hidden">
-                  facebook
-                </div>
-                <div class="h-12 w-12 bg-blue-400">
-
-                </div>
-              </div>
-            </li>
-          </ul>
+  <div id="sticky-error-message" style="top: 240px;" class="hidden lg:flex fixed z-50 right-0">
+    <ul>
+      <li>
+        <div class="flex">
+          <div class="invisible flex px-4 bg-blue-400 text-white rounded-l w-32 border-r">
+            <p class="text-black font-semibold m-auto tracking-wider">facebook</p>
+          </div>
+          <div class="h-10 w-10 bg-blue-400">
+            <img src="/img/logos/facebook.png" class="p-1 cursor-pointer" alt="facebook logo">
+          </div>
         </div>
+      </li>
+      <li>
+        <div class="flex mt-1">
+          <div class="invisible flex px-4 bg-blue-400 text-white rounded-l w-32 border-r">
+            <p class="text-black font-semibold m-auto tracking-wider">phone</p>
+          </div>
+          <div class="h-10 w-10 bg-blue-400">
+            <img src="/img/logos/phone.png" class="p-2 cursor-pointer" alt="phone logo">
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="flex mt-1">
+          <div class="invisible flex px-4 bg-blue-400 text-white rounded-l w-32 border-r">
+            <p class="text-black font-semibold m-auto tracking-wider">mail</p>
+          </div>
+          <div class="h-10 w-10 bg-blue-400">
+            <img src="/img/logos/mail.png" class="p-1 cursor-pointer" alt="mail logo">
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+
+
+<div class="fixed z-50 inset-x-0 bottom-0 mb-4 w-full lg:hidden flex">
+  <ul class="mx-auto flex flex-row">
+    <li>
+      <div class="flex">
+        <div class="h-10 w-10 bg-blue-400 rounded">
+          <img src="/img/logos/facebook.png" class="p-1 cursor-pointer" alt="facebook logo">
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="flex ml-2">
+        <div class="h-10 w-10 bg-blue-400 rounded">
+          <img src="/img/logos/phone.png" class="p-2 cursor-pointer" alt="phone logo">
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="flex ml-2">
+        <div class="h-10 w-10 bg-blue-400 rounded">
+          <img src="/img/logos/mail.png" class="p-1 cursor-pointer" alt="mail logo">
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
