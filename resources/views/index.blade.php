18b38cc9b32ba6fc23aec0655588f1e269dc45f6
Luke Joseph <illuzual@gmail.com>
2019-08-10 15:21:11 +0200

    reordered the index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
   <div class="px-3">
     <div id="hero-image" class="bg-cover bg-center" style="background-image: url('/img/hero-images/index-image.jpg'); max-width: 100%; width: 100%;">
       <div class="flex flex-col h-full">
-        <img src="/img/urbaniks-plain-ws-no-tm.png" alt="urbaniks logo" class="w-64 mx-auto mt-6">
+        <img src="/img/urbaniks-plain-ws-no-tm.png" alt="urbaniks logo" class="w-64 mx-auto mt-12">
         <div class="mx-auto text-white uppercase flex flex-col">
           <h1 class="text-xl lg:text-xl text-center w-full mx-auto  px-2" style="background-color: rgba(0,0,0,0.4);">Street Skool Of Dance</h1>
           {{-- <p class="hidden sm:flex text-center italic text-2xl mx-auto px-2 mt-4" style="background-color: rgba(49,49,67,0.4);">creativity in movement</p> --}}
@@ -35,6 +35,44 @@
       </div>
     </div>
 
+  </div>
+
+  <div class="flex flex-col mt-24 mb-24">
+    <div class="w-1/2 mx-auto mb-4">
+      {{-- <img src="/img/logos/urbanikslogobuildings-white.png" alt=""> --}}
+    </div>
+    <h2 class="w-5/6 mx-auto text-2xl lg:text-3xl text-center uppercase text-white tracking-wide mb-8">Our Experience</h2>
+    <div class="flex flex-col md:flex-row w-5/6 mx-auto">
+
+      <div class="md:w-1/2 p-4">
+        <h3 class="text-2xl text-gray-100 mb-4">Est. Since 2001</h3>
+        <p class="mb-0 text-gray-200 mb-4 sm:w-11/12">We are one of the first schools that appeared on the scene all the way back from 2001. We have
+        successfully taught over 200 pupils from all backgrounds, ages and skill level.</p>
+        <p class="mb-0 text-gray-200 sm:w-11/12">
+          Dance is great way to stay healthy, build confidence & interact with peers while having fun in this growing digital age.
+        </p>
+      </div>
+
+      <div class="md:w-1/2 p-4">
+        <h3 class="text-2xl text-gray-100 mb-4">Our Experience</h3>
+        <p class="mb-0 text-gray-200">
+          We have over 30 years experience as professional dancers, choreographers, directors, teachers, judges and lecturers in dance.
+          <span class="text-lightblue-600">To learn more about the teachers</span>
+           <a href="/teachers" class="bg-lightblue-300 text-black font-bold rounded px-2 text-xs">click here </a>
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex flex-col mt-16">
+    <div class="w-32 mx-auto mb-4">
+      <img src="/img/icons/Break_dance_light.png" alt="">
+    </div>
+    <h2 class="text-2xl lg:text-3xl text-center uppercase text-white tracking-wide mb-8">Our Mission</h2>
+    <p class="sm:text-center text-gray-200 w-5/6 sm:w-2/3 mx-auto text-lg p-4">We guide our pupils of all ages to enjoy dance and movement. We provide a supportive place of expression where students can learn the art
+      of dance, develop confidence and life long friendships. We bring togetherness and community with lots of interaction through dance.
+
+    </p>
   </div>
 
   <div class="flex flex-col">
@@ -79,44 +117,6 @@
 
   </div>
 
-  <div class="flex flex-col mt-16">
-    <div class="w-32 mx-auto mb-4">
-      <img src="/img/icons/Break_dance_light.png" alt="">
-    </div>
-    <h2 class="text-2xl lg:text-3xl text-center uppercase text-white tracking-wide mb-8">Our Mission</h2>
-    <p class="sm:text-center text-gray-200 w-5/6 sm:w-2/3 mx-auto text-lg p-4">We guide our pupils of all ages to enjoy dance and movement. We provide a supportive place of expression where students can learn the art
-      of dance, develop confidence and life long friendships. We bring togetherness and community with lots of interaction through dance.
-
-    </p>
-  </div>
-
-  <div class="flex flex-col mt-24 mb-24">
-    <div class="w-1/2 mx-auto mb-4">
-      {{-- <img src="/img/logos/urbanikslogobuildings-white.png" alt=""> --}}
-    </div>
-    <h2 class="w-5/6 mx-auto text-2xl lg:text-3xl text-center uppercase text-white tracking-wide mb-8">The Original Street Skool Of Dance</h2>
-    <div class="flex flex-col md:flex-row w-5/6 mx-auto">
-
-      <div class="md:w-1/2 p-4">
-        <h3 class="text-2xl text-gray-100 mb-4">Est. Since 2001</h3>
-        <p class="mb-0 text-gray-200 mb-4 sm:w-11/12">We are one of the first schools that appeared on the scene all the way back from 2001. We have
-        successfully taught over 200 pupils from all backgrounds, ages and skill level.</p>
-        <p class="mb-0 text-gray-200 sm:w-11/12">
-          Dance is great way to stay healthy, build confidence & interact with peers while having fun in this growing digital age.
-        </p>
-      </div>
-
-      <div class="md:w-1/2 p-4">
-        <h3 class="text-2xl text-gray-100 mb-4">Our Experience</h3>
-        <p class="mb-0 text-gray-200">
-          We have over 30 years experience as professional dancers, choreographers, directors, teachers, judges and lecturers in dance.
-          <span class="text-lightblue-600">To learn more about the teachers</span>
-           <a href="/teachers" class="bg-lightblue-300 text-black font-bold rounded px-2 text-xs">click here </a>
-        </p>
-      </div>
-    </div>
-  </div>
-
   <div class="flex w-full mt-24 py-8 bg-gray-100 mb-16">
     <div class="flex m-auto w-10/12 md:w-auto">
 
@@ -124,7 +124,7 @@
         <p class="mb-0 text-2xl italic text-lightblue-1000">Ready To Learn Street Dance?</p>
       </div>
       <a href="/registration" class="">
-        <button class="py-2 px-4 bg-red-400 rounded-lg text-red-900 uppercase font-bold tracking-wide shadow-lg mx-auto" type="button">register today</button>
+        <button class="py-2 px-4 bg-red-400 rounded-lg text-red-900 uppercase font-bold tracking-wide shadow-lg mx-auto" type="button">contact us today</button>
       </a>
 
     </div>
